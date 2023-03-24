9c15b0e6c237074f521b5112b4d5cee6a25e2b15
levy1503 <yoav.levy.ace@gmail.com>
2023-03-24 12:29:21 +0300

    friday 12:29

--- app/src/main/java/services/Auth.java
+++ app/src/main/java/services/Auth.java
@@ -1,15 +1,29 @@
 package services;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Auth {
     private FirebaseAuth mAuth;
     private static Auth instance;
     FirebaseUser currentUser;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
 
     private Auth() {
@@ -47,19 +61,39 @@ public class Auth {
     public Task<AuthResult> signUp(String email, String password, String userName , String phoneNumber ) {
 
         // sign up with email and password
-        return  mAuth.createUserWithEmailAndPassword(email, password)
+        Map<String, Object> user = new HashMap<>();
+        return mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        // add to firestore
-
-                        // save current user
                         currentUser = mAuth.getCurrentUser();
+                        // add to firestore
+                        user.put("email", email);
+                        user.put("name", userName);
+                        user.put("phone", phoneNumber);
+                        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getApplicationContext(), "User added successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(), "Error adding user", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        // save current user
+
                     } else {
 
                         // return error message to user
                         currentUser = null;
                     }
                 });
+    }
+
+    private Context getApplicationContext() {
+        return null;
     }
 
 
